72fbcdb08870a889ce3a77fcd99598efc252e1c4
sue-tax <119954167+sue-tax@users.noreply.github.com>
2023-03-10 20:59:10 +0900

    Update nanishi.bat

--- nanishi/nanishi.bat
+++ nanishi/nanishi.bat
@@ -1,1 +1,1 @@
-java -jar C:\Users\XXXXXXXX\AppData\Roaming\Microsoft\Windows\SendTo\nanishi.jar C:\Users\XXXXXXXX\AppData\Roaming\Microsoft\Windows\SendTo\config.csv %1
+java -jar C:\Users\XXXXXXXX\AppData\Roaming\Microsoft\Windows\SendTo\nanishi.jar C:\Users\XXXXXXXX\AppData\Roaming\Microsoft\Windows\SendTo\config.csv %1 %2 %3 %4 %5 %6 %7 %8 %9
