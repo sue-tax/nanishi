3fd964c4ae1c6dd674f261798834aa25ef6429ba
sue-tax <119954167+sue-tax@users.noreply.github.com>
2023-03-19 15:12:54 +0900

    BOM対策

--- nanishi/src/nanishi/ConfigProc.java
+++ nanishi/src/nanishi/ConfigProc.java
@@ -15,7 +15,6 @@ package nanishi;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -91,6 +90,116 @@ public class ConfigProc {
 		this.analysis = analysis;
 	}
 
+//	public String readConfig() {
+//		D.dprint_method_start();
+//		String strRet = null;
+//		// https://uxmilk.jp/48018
+//		File f = new File(strFileConfig);
+//		FileReader fr;
+//		try {
+//			fr = new FileReader(f, StandardCharsets.UTF_8);
+//		} catch (Exception e) {
+//			strRet = String.format(
+//					"Cannot_open_file_%s_"
+//					+ "ファイルがオープンできません",
+//					strFileConfig);
+//			D.dprint(strRet);
+//			D.dprint_method_end();
+//			return strRet;
+//		}
+//		BufferedReader br = new BufferedReader(fr);
+//
+//		String line;
+//		List<String> lines = new ArrayList<String>();
+//		// 1行ずつCSVファイルを読み込む
+//		try {
+//			while ((line = br.readLine()) != null) {
+//				lines.add(line);
+//			}
+//			br.close();
+//		} catch (IOException e) {
+//			strRet = String.format(
+//					"Cannot_read_file_%s_"
+//					+ "ファイルが読めません",
+//					strFileConfig);
+//			D.dprint(strRet);
+//			D.dprint_method_end();
+//			return strRet;
+//		}
+//        D.dprint(lines);
+//        for (int iLine = 0; iLine < lines.size(); iLine++) {
+//        	D.dprint(lines.get(iLine));
+//            String[] aData = lines.get(iLine).split(",");
+//            D.dprint(aData[0]);
+//            if (aData[0].matches("[1-9]")) {
+//            	strRet = readMatch(iLine, aData);
+//        		if (strRet != null) {
+//        			D.dprint(strRet);
+//        			D.dprint_method_end();
+//        			return strRet;
+//        		}
+//            } else if (aData[0].equals("0")) {
+//            	if (aData.length < 2) {
+//        			strRet = String.format(ERROR_TOOFEW_COLUMN,
+//        					iLine+1, 2);
+//        			D.dprint(strRet);
+//        			D.dprint_method_end();
+//        			return strRet;
+//            	}
+//            	if (aData[1].equals(VERSION)) {
+//            		strRet = readVersion(iLine, aData);
+//            		if (strRet != null) {
+//            			D.dprint(strRet);
+//            			D.dprint_method_end();
+//            			return strRet;
+//            		}
+//            	} else if (aData[1].equals(FILENAME_FORMAT)) {
+//            		strRet = readFormat(iLine, aData);
+//            		if (strRet != null) {
+//            			D.dprint(strRet);
+//            			D.dprint_method_end();
+//            			return strRet;
+//            		}
+//            	} else if (aData[1].equals(OPTION)) {
+//            		strRet = readOption(iLine, aData);
+//            		if (strRet != null) {
+//            			D.dprint(strRet);
+//            			D.dprint_method_end();
+//            			return strRet;
+//            		}
+//            	} else {
+//            		strRet = String.format(
+//            				"Invalid_Line-%d_%s_"
+//            				+ "２列目が正しくない",
+//            				iLine+1, aData[1]);
+//        			D.dprint(strRet);
+//        			D.dprint_method_end();
+//        			return strRet;
+//            	}
+//            } else {
+//        		strRet = String.format(
+//        				"InvalidMatchNumber_Line-%d_%s_"
+//        				+ "１列目のマッチ番号が正しくない",
+//        				iLine+1, aData[0]);
+//        		continue;
+////    			D.dprint(strRet);
+////    			D.dprint_method_end();
+////    			return strRet;
+//            }
+//        }
+//        if (strFileFormat == null) {
+//    		strRet = String.format(
+//    				"NoFileFormat_"
+//    				+ "変更ファイル名フォーマットの指定がない");
+//			D.dprint(strRet);
+//			D.dprint_method_end();
+//			return strRet;
+//        }
+//        D.dprint(strRet);
+//        D.dprint_method_end();
+//        return strRet;
+//	}
+
 	public String readConfig() {
 		D.dprint_method_start();
 		String strRet = null;
@@ -110,8 +219,10 @@ public class ConfigProc {
         // CSVファイルの読み込み
         List<String> lines;
 		try {
-			lines = Files.readAllLines(
-					path, Charset.forName("UTF-8"));
+//			lines = Files.readAllLines(
+//					path, Charset.forName("UTF-8"));
+			lines = Files.readAllLines(path);
+//					path, Charset.forName("UTF-8"));
 		} catch (IOException e1) {
 			strRet = String.format(
 					"CannotReadFile_Files.readAllLines_%s"
@@ -122,7 +233,26 @@ public class ConfigProc {
 			return strRet;
 		}
 //        D.dprint(lines);
-        for (int iLine = 0; iLine < lines.size(); iLine++) {
+
+//		public static final ByteOrderMark UTF_8
+//		= new ByteOrderMark("UTF-8", 0xEF, 0xBB, 0xBF);
+
+		String firstLine = lines.get(0);
+//        D.dprint(String.format("%02x", (int)(firstLine.charAt(0))));
+//        D.dprint(String.format("%02x", (int)(firstLine.charAt(1))));
+//        D.dprint(String.format("%02x", (int)(firstLine.charAt(2))));
+		// BOM対策
+        if ((int)(firstLine.charAt(0)) == 0xfeff) {
+			lines.set(0, firstLine.substring(1));
+		}
+//		if ((firstLine.charAt(0) == 0xEF)
+//				&& (firstLine.charAt(1) == 0xBB)
+//				&& (firstLine.charAt(2) == 0xBF)) {
+//			lines.set(0, firstLine.substring(3));
+//		}
+//        D.dprint(lines);
+
+		for (int iLine = 0; iLine < lines.size(); iLine++) {
         	D.dprint(lines.get(iLine));
             String[] aData = lines.get(iLine).split(",");
             D.dprint(aData[0]);
@@ -194,7 +324,6 @@ public class ConfigProc {
         D.dprint_method_end();
         return strRet;
 	}
-
 
 	private String readMatch( int iLine, String[] aData ) {
 		D.dprint_method_start();
